2eaa72dfaf60f40c28d8908189d1f9dfec84956c
my0sot1s <manhte231@gmail.com>
2017-08-10 03:31:15 +0700

    initialCommit

---
+++ Funny/index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import Funny from './src/app'
+
+
+AppRegistry.registerComponent('Funny', () => Funny);
---
+++ Funny/src/app.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { View, Image, StyleSheet, Dimensions, StatusBar, ListView } from 'react-native'
+import Card from './modules/Card';
+import Ex from './modules/Ex'
+import Interactable from 'react-native-interactable';
+const { height, width } = Dimensions.get("screen")
+
+class BaseApp extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows([{ color: '#ba68c8' }, { color: '#52a43a' }, { color: '#f7aa17' }, { color: '#ef5350' }]),
+    };
+  }
+  renderRow(data) {
+    return (
+      <Interactable.View
+        horizontalOnly={true}
+        snapPoints={[{ x: 360 }, { x: 0 }, { x: -360 }]}>
+        <Card color={data.color} />
+      </Interactable.View>
+    );
+  }
+  render() {
+    return (
+      <View>
+        <StatusBar hidden={true} />
+        <App>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow.bind(this)}
+          />
+          {/* <Ex /> */}
+        </App>
+      </View>
+    );
+  }
+}
+
+
+const App = props => (
+  <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 100 + '%', height, backgroundColor: "#525A75" }}>
+    {props.children}
+  </View>
+)
+const styles = StyleSheet.create({
+  main: {
+
+  }
+});
+export default BaseApp;
